4421cd9e93f30048206be94c95041deab1c8a311
Wummysoft <kaseem.wummy@gmail.com>
2022-12-22 10:29:34 -0800

    strcat

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * _strcat - concatenates two strings
+ * @dest: copy to
+ * @src: copy from
+ * Return: pointer to dest
+ */
+char *_strcat(char *dest, char *src)
+{
+	int i;
+	int l;
+
+	i = 0;
+	while (dest[i] != '\0')
+	{
+		i++;
+	}
+	l = 0;
+	while (src[l] != '\0')
+	{
+		dest[i] = src[l];
+		i++;
+		l++;
+	}
+	dest[i] = '\0';
+	return (dest);
+}
