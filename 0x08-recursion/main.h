c794a4141e2818b4c967ab952a100951fa680bc7
Wummysoft <kaseem.wummy@gmail.com>
2023-01-05 12:02:31 -0800

    Dreams feel real while we're in them. It's only when we wake up that we realize something was actually strange

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -4,6 +4,8 @@
 int _putchar(char c);
 void _puts_recursion(char *s);
 void _print_rev_recursion(char *s);
+int pal_checker(char *s, int i, int j);
+int is_palindrome(char *s);
 int _strlen_recursion(char *s);
 int factorial(int n);
 int _pow_recursion(int x, int y);
