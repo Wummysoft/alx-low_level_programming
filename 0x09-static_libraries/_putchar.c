502ea9ff1f7613b524180093f536da4f01caf066
Wummysoft <kaseem.wummy@gmail.com>
2023-01-06 03:41:24 -0800

    README file and main.h

---
+++ 0x09-static_libraries/_putchar.c
@@ -0,0 +1,11 @@
+#include "main.h"                                                         
+#include <unistdio.h>
+/**
+ * _putchar -write the character c to stdout
+ * @c: The character to print
+ * return: on success 1
+ */
+int char
+{
+	return
+}
