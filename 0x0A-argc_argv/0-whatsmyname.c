56aad22f0cd62961d36a43ad1599eb74ab73d0e7
Wummysoft <kaseem.wummy@gmail.com>
2023-01-06 19:24:49 -0800

    It ain't what they call you, it's what you answer to

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - Print the name of the progress
+ * @argc: Count arguments
+ * @argv: Arguments
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(int argc, char *argv[])
+{
+(void) argc:
+	print("%s\n", argv[0]);
+
+return (0);
+}
