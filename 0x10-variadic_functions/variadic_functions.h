a72cfdda7462ffe5eabc71c59770a25ca37fe317
Wummysoft <kaseem.wummy@gmail.com>
2023-01-19 15:57:10 -0800

    update

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,9 +1,9 @@
 #ifndef _VARIADIC_H
 #define _VARIADIC_H
 
-Int _putchar(char c);
-Int sum_them_all(const unsigned int n, …);
-Void print_numbers(const char *separator, const unsigned int n, …);
-Void print_strings(const char *separator, const unsigned int n, …);
-Void print_all(const char * const format, …);
+int _putchar(char c);
+int sum_them_all(const unsigned int n, ...);
+void print_numbers(const char *separator, const unsigned int n, ...);
+void print_strings(const char *separator, const unsigned int n, ...);
+void print_all(const char * const format, ...);
 #endif
