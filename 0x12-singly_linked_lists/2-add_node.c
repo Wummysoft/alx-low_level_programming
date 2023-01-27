169179cece6e461b84debaf3009e75f9f01a558e
Wummysoft <kaseem.wummy@gmail.com>
2023-01-26 20:44:42 -0800

    update

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -14,7 +14,7 @@ list_t *add_node(list_t **head, const char *str)
 	list_t *newNode;
 	unsigned int length = 0;
 
-	newNode = malloc(sizeof(list_t));
+		newNode = malloc(sizeof(list_t));
 	if (newNode == NULL)
 	{
 		free(newNode);
