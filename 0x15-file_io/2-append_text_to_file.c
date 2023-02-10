3a1fbfc28438584e4dd181e4e2f6a93058478647
Wummysoft <kaseem.wummy@gmail.com>
2023-02-10 15:43:40 -0800

    2. Speak gently, she can hear

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -1,11 +1,9 @@
 #include "main.h"
 
 /**
- *
  * append_text_to_file - appends text at the end of a file
  * @filename: filename.
  * @text_content: added content.
- *
  * Return: 1 if the file exists. -1 if the fails does not exist
  * or if it fails.
  */
@@ -25,7 +23,7 @@ int append_text_to_file(const char *filename, char *text_content)
 	if (text_content)
 	{
 		for (nletters = 0; text_content[nletters]; nletters++)
-			;
+		;
 		rwr = write(fd, text_content, nletters);
 		if (rwr == -1)
 			return (-1);
